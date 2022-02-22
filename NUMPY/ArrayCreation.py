5406595b16fcc906a35eb14f19e196d0abd09e2e
willandru <willandru@hotmail.com>
2022-02-22 12:25:15 -0500

    NUmpy Library

---
+++ NUMPY/ArrayCreation.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+a=np.array([2,3,4])
+print(a)
+
+b=np.array([1.2,3.5,5.1])
+print(b)
+
+c= np.array([(1.5,2,3),(4,5,6)])
+print(c)
+
+tablero= np.array([(0,0,0),(0,0,1)])
+print(tablero)
+
+d=np.zeros((10,3))
+print(d)
+
+e=np.ones((2,3,3), dtype=np.int16)
+print(e)
+
+np.empty((2,3))
+
+np.arange(10,30,5)
+
+np.linspace(0,2,9)
---
+++ NUMPY/BasicOperations.py
@@ -0,0 +1,17 @@
+import numpy as np
+print('The basics')
+a=np.array([20,30,40,50])
+b=np.arange(4)
+print(b)
+
+c= a-b 
+print(c)
+
+print(b**2)
+
+print(10*np.sin(a))
+
+print(a<35)
+
+print('Matrix Multipliaction')
+
