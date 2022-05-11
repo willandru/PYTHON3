30e663ea024bca4db9d8a25107ed6d9136718b69
willandru <willandru@hotmail.com>
2022-05-11 16:00:27 -0500

    DOing that

--- MATPLOTLIB/animated_charts.py
+++ MATPLOTLIB/animated_charts.py
@@ -1,4 +1,4 @@
-import numpy as numpy
+import numpy as np
 import matplotlib.pyplot as plt
 
 labels = ["Chrome", "Firefox", "Safari"]
@@ -13,6 +13,17 @@ shares = np.array([
 	[0.3, 0.5, 0.2]
 ])
 
-years = [2010 + i for i iun range(8)]
+years = [2010 + i for i in range(8)]
 
 
+plot_steps= np.array([shares[0]])
+
+last_shares= shares[0]
+for i in range(1, len(shares)):
+	differences = shares[i] -last_shares
+	differences /= 100
+
+	for j in range(100):
+		plot_steps = np.append(plot_steps, [last_shares+differences *j], 0)
+
+print(plot_steps)
