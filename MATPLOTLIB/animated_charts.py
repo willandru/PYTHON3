10a75742b714680d2cad6bf2bb215fea2a40595c
willandru <willandru@hotmail.com>
2022-05-11 11:16:13 -0500

    Matplotlib- Creating ANIMATED charts

---
+++ MATPLOTLIB/animated_charts.py
@@ -0,0 +1,18 @@
+import numpy as numpy
+import matplotlib.pyplot as plt
+
+labels = ["Chrome", "Firefox", "Safari"]
+shares = np.array([
+	[0.5, 0.3, 0.2],
+	[0.4, 0.4, 0.2],
+	[0.4, 0.1, 0.5],
+	[0.5, 0.2, 0.3],
+	[0.3, 0.2, 0.5],
+	[0.3, 0.4, 0.3],
+	[0.2, 0.3, 0.5],
+	[0.3, 0.5, 0.2]
+])
+
+years = [2010 + i for i iun range(8)]
+
+
